90dc9347f0df40246bbaa343569542d0558d25d6
Kento Lauzon <kento.lauzon@ligature.ca>
2022-09-15 19:51:31 +0100

    friendlier error message

--- ohdieux/service/manifest_service.py
+++ ohdieux/service/manifest_service.py
@@ -5,6 +5,7 @@ from ohdieux.model.programme import Programme
 
 from ohdieux.ohdio.ohdio_reader import OhdioReader
 from ohdieux.ohdio.ohdio_reader_v2 import OhdioReaderV2
+from ohdieux.service.exceptions import ManifestGenerationException
 
 
 @Component
@@ -17,6 +18,7 @@ class ManifestService(object):
         self._logger = logging.getLogger(self.__class__.__name__)
 
     def generate_podcast_manifest(self, programme_id: str, reverse_segments: bool) -> Programme:
+        exceptions = []
         for reader in [self._reader_v2, self._ohdio_reader]:
             try:
                 res = reader.query(programme_id, reverse_segments)
@@ -25,6 +27,8 @@ class ManifestService(object):
                     continue
                 return res
             except Exception as e:
+                exceptions.append(e)
                 self._logger.warning(f"Failed to generate manifest for programme {programme_id} using {reader.__class__} reader.", e)
 
-            return self._ohdio_reader.query(programme_id, reverse_segments)
+            self._logger.error(f"Failed to generate manifest for programme {programme_id}.")
+            raise ManifestGenerationException(f"Uncaught error while generating manifest for programme {programme_id}. {exceptions}")
