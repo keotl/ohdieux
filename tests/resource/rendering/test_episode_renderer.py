25573ce94a3db98c9bc3e29231378dab7eaef482
Kento Lauzon <kento@lauzon.co.uk>
2024-07-06 08:44:16 +0100

    fix unit tests

--- tests/resource/rendering/test_episode_renderer.py
+++ tests/resource/rendering/test_episode_renderer.py
@@ -46,7 +46,7 @@ class EpisodeRendererTests(unittest.TestCase):
             MediaDescriptor("example.com/mp4/file.mp4", "", 1),
             MediaDescriptor("untouched", "", 2)
         ], False)
-        programme = Programme(PROGRAMME.programme, [episode], datetime.now())
+        programme = Programme(PROGRAMME.programme, [episode], datetime.now(), ordering="unknown")
         render = renderer(favor_aac=True)
 
         # When
@@ -61,7 +61,7 @@ class EpisodeRendererTests(unittest.TestCase):
     def test_render_limit_number_of_episodes(self):
         # Given
         programme = Programme(PROGRAMME.programme, [EPISODE for _ in range(100)],
-                              datetime.now())
+                              datetime.now(), ordering="unknown")
         render = renderer(limit_episodes=True)
 
         # When
@@ -77,7 +77,7 @@ class EpisodeRendererTests(unittest.TestCase):
             "episode title", "description", "episodeid", datetime.now(), 123,
             [MediaDescriptor("first", "", 1),
              MediaDescriptor("second", "", 2)], True)
-        programme = Programme(PROGRAMME.programme, [replay], datetime.now())
+        programme = Programme(PROGRAMME.programme, [replay], datetime.now(), ordering="unknown")
         render = renderer(exclude_replays=True)
 
         # When
@@ -96,4 +96,4 @@ EPISODE = EpisodeDescriptor(
 PROGRAMME = Programme(programme=ProgrammeDescriptor("title", "description", "author",
                                                     "link", "image_url"),
                       episodes=[EPISODE],
-                      build_date=datetime.now())
+                      build_date=datetime.now(), ordering="unknown")
